b313bd7dd882c5d61611a8d35b003d56e6aa1784
Gamalliel Sharon <you@example.com>
2021-11-01 18:23:25 +0700

    styling detail page

--- supapark/src/pages/About.js
+++ supapark/src/pages/About.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className='my-20 mx-15'>
+      <h1 className='text-center mb-15'>Tentang Aplikasi</h1>
+      <p>
+        Aplikasi ini dibuat untuk menemukan lokasi skatepark terdekat
+        berdasarkan lokasi pengguna. <br /> Metode yang digunakan pada aplikasi
+        ini adalah metode Haversine Formula. Metode Haversine <br /> Formula di
+        gunakan untuk meng-kalkulasi jarak dari titik geografis pengguna dan
+        titik geografis skatepark yang ingin dituju.
+      </p>
+    </div>
+  );
 };
 
 export default About;
