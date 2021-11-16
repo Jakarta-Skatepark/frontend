5f8028dd5046a1cf16b2f14b34de20da6c15b008
Gamalliel Sharon <you@example.com>
2021-11-16 11:41:30 +0700

    adding marker to map detail and adding form

--- supapark/src/components/MapBox.js
+++ supapark/src/components/MapBox.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
+// import mapboxgl from "mapbox-gl";
+// import "mapbox-gl/dist/mapbox-gl.css";
+// import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+// import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
 
-const MapBox = ({ userLatitude, userLongitude }) => {
+const MapBox = ({
+  userLatitude,
+  userLongitude,
+  parkLatitude,
+  parkLongitude,
+  parkName,
+}) => {
   const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-  const [viewport] = useState({
+  const [viewport, setViewport] = useState({
     latitude: userLatitude,
     longitude: userLongitude,
-    zoom: 10,
+    zoom: 9,
   });
+
+  const parkMarker = (
+    <Marker latitude={parkLatitude} longitude={parkLongitude}>
+      <button className='py-2 px-2 bg-primary-3 rounded-7 text-primary-2'>
+        {parkName}
+      </button>
+    </Marker>
+  );
+
+  const userMarker = (
+    <Marker latitude={userLatitude} longitude={userLongitude}>
+      <button className='py-2 px-2 bg-primary-3 rounded-7 text-primary-2'>
+        You
+      </button>
+    </Marker>
+  );
+
   return (
     <div className='map-container'>
       <ReactMapGL
@@ -16,7 +43,11 @@ const MapBox = ({ userLatitude, userLongitude }) => {
         mapStyle='mapbox://styles/gmllshrn/ckvj33j018nc515o2pcsxdbdj'
         width='100%'
         height='500px'
-      ></ReactMapGL>
+        onViewportChange={(viewport) => setViewport(viewport)}
+      >
+        {parkMarker}
+        {userMarker}
+      </ReactMapGL>
     </div>
   );
 };
--- supapark/src/pages/Login.js
+++ supapark/src/pages/Login.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <h1>Silahkan Masuk Untuk Admin</h1>
+      <form action=''>
+        <div className='flex flex-col mt-10 gap-2'>
+          <label>Email</label>
+          <input className='inputbox' type='text' />
+        </div>
+        <div className='flex flex-col mt-10 gap-2'>
+          <label>Password</label>
+          <input className='inputbox' type='password' />
+        </div>
+        <button className='btn mt-14'>Submit</button>
+      </form>
+    </div>
+  );
 }
